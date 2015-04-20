164272eb52e2f72e902aaf3c0b0d512e48178c4e
Ivan Reese <ivan@cdiginc.com>
2015-04-20 15:27:47 +0200

    Add lein :user profile

---
+++ .lein/profiles.clj
@@ -0,0 +1,5 @@
+{:user {:plugins [[lein-autoexpect "1.4.0"]
+                  [lein-expectations "0.0.7"]
+                  [lein-pprint "1.1.2"]]
+        :dependencies [[slamhound "1.5.5"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
