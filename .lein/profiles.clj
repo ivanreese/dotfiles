95c62bf6ae808143f01be8b64ece9aa8f2d7c471
Ivan Reese <ivan@cdiginc.com>
2015-04-20 16:13:29 +0200

    Add lein-kibit

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,6 +2,7 @@
                   [lein-expectations "0.0.8"]
                   [lein-pprint "1.1.2"]
                   [lein-ancient "0.6.7"]
-                  [lein-plz "0.4.0-SNAPSHOT" :exclusions [[rewrite-clj] [ancient-clj]]]]
+                  [lein-plz "0.4.0-SNAPSHOT" :exclusions [[rewrite-clj] [ancient-clj]]]
+                  [lein-kibit "0.1.2"]]
         :dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
