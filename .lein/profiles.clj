1fcecf0781bcace1fbe728d00b764e5b1ff29a50
Ivan Reese <ivan@cdiginc.com>
2015-04-20 15:53:37 +0200

    Add a few more lein plugins, why not

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,7 @@
 {:user {:plugins [[lein-autoexpect "1.4.0"]
                   [lein-expectations "0.0.7"]
-                  [lein-pprint "1.1.2"]]
+                  [lein-pprint "1.1.2"]
+                  [lein-ancient "0.6.7"]
+                  [lein-plz "0.4.0-SNAPSHOT" :exclusions [[rewrite-clj] [ancient-clj]]]]
         :dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
