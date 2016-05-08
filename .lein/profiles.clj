423e3109bf3d54d844081cad2f9ef4803eb08f85
Ivan Reese <ivanreese@gmail.com>
2016-05-07 18:50:44 -0600

    Update lein-ancient

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user {:plugins [[lein-autoexpect "1.4.2"]
                   [lein-expectations "0.0.8"]
                   [lein-pprint "1.1.2"]
-                  [lein-ancient "0.6.7"]
+                  [lein-ancient "0.6.10"]
                   [lein-plz "0.4.0-SNAPSHOT" :exclusions [[rewrite-clj] [ancient-clj]]]
                   [lein-kibit "0.1.2"]]
         :dependencies [[slamhound "1.5.5"]]
