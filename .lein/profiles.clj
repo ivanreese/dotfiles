1c42918aab2830d630be3926d0f1d2095f37b035
Ivan Reese <ivanreese@gmail.com>
2017-05-30 23:19:15 -0600

    Add lein-count

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,9 @@
-{:user {:plugins [[lein-autoexpect "1.9.0"]
+{:user {:plugins [[lein-ancient "0.6.10"]
+                  [lein-autoexpect "1.9.0"]
+                  [lein-count "1.0.3"]
                   [lein-expectations "0.0.8"]
-                  [lein-pprint "1.1.2"]
-                  [lein-ancient "0.6.10"]
+                  [lein-kibit "0.1.2"]
                   [lein-plz "0.4.0-SNAPSHOT" :exclusions [[rewrite-clj] [ancient-clj]]]
-                  [lein-kibit "0.1.2"]]
+                  [lein-pprint "1.1.2"]]
         :dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
