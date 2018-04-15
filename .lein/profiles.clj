33b93ae7529a12b06a7e0bbf6c10bf4d14fb25d0
Ivan Reese <ivanreese@gmail.com>
2018-04-14 19:43:51 -0600

    Update lein user profile plugin versions

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,9 +1,9 @@
-{:user {:plugins [[lein-ancient "0.6.10"]
+{:user {:plugins [[lein-ancient "0.6.15"]
                   [lein-autoexpect "1.9.0"]
-                  [lein-count "1.0.3"]
+                  [lein-count "1.0.8"]
                   [lein-expectations "0.0.8"]
-                  [lein-kibit "0.1.2"]
+                  [lein-kibit "0.1.5"]
                   [lein-plz "0.4.0-SNAPSHOT" :exclusions [[rewrite-clj] [ancient-clj]]]
-                  [lein-pprint "1.1.2"]]
+                  [lein-cprint "1.3.0"]]
         :dependencies [[slamhound "1.5.5"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
